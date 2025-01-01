d496e05ef2cca7c631c6dee2c7cd7d4802ff98f3
Alessandro <alessandro2ap3@gmail.com>
2025-01-01 20:00:27 +0100

    added control and store data file

--- profit_calculator.go
+++ profit_calculator.go
@@ -1,35 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	revenue := prinText("Revenue: ")
-	expenses := prinText("Expenses: ")
-	taxRate := prinText("Tax Rate: ")
+	revenue, err := getUserInput("Revenue: ")
+
+	if err != nil {
+		fmt.Println(err)
+		//panic(err)
+		return
+	}
+
+	expenses, err := getUserInput("Expenses: ")
+
+	if err != nil {
+		fmt.Println(err)
+		//panic(err)
+		return
+	}
+
+	taxRate, err := getUserInput("Tax Rate: ")
+
+	if err != nil {
+		fmt.Println(err)
+		//panic(err)
+		return
+	}
 
 	ebt, profit, ratio := calculator(revenue, expenses, taxRate)
 
-	formattedEbt := fmt.Sprintf("ebt: %.2f\n", ebt)
-	formattedProfit := fmt.Sprintf("profit: %.2f\n", profit)
-	formattedRatio := fmt.Sprintf("ratio: %.2f\n", ratio)
+	fmt.Printf("%.2f\n", ebt)
+	fmt.Printf("%.2f\n", profit)
+	fmt.Printf("%.2f\n", ratio)
 
-	fmt.Print(formattedEbt, formattedProfit, formattedRatio)
-
+	storeResults(ebt, profit, ratio)
 }
 
-func prinText(infoText string) float64 {
+func storeResults(ebt, profit, ratio float64) {
+	numText := fmt.Sprintf("EBT: %.2f\nProfit: %.2f\nRatio: %.2f", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(numText), 0644)
+}
+
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
 
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("value not valid")
+	}
+
+	return userInput, nil
 }
 
 func calculator(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
 	ebt = revenue - expenses
 	profit = ebt * (1 - taxRate/100)
 	ratio = ebt / profit
+
 	return
 }
